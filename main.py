5097b09f0a53783b696f9b02e80d65be2379585d
BartlomiejLesniowski <lesionauka@gmail.com>
2025-01-10 13:12:20 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import csv
+from datetime import datetime
+
+class CSV:
+    FILE = "fincance_file.csv"
+    COLUMNS = ["Date", "Type", "Amount", "Description"]
+
+    def create_csv(cls):
+        try:
+            pd.read_csv(cls.FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.FILE, index=False)
+
+    def add_entry(cls, date, type, amount, description):
+        new_entry = {
+            "Date" : date,
+            "Type" : type,
+            "Amount" : amount,
+            "Description" : description
+        }
+        with open(cls.FILE, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+    
+
